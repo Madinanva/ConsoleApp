e9d12fb67b287763e2a00ef95cc074f65097bc2d
Madina <madinarn@code.edu.az>
2022-07-26 09:47:48 +0400

    third commit

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -11,81 +11,101 @@ namespace Manage
         static void Main()
         {
             GroupController _groupController = new GroupController();
-            StudentController _studentController = new StudentController(); 
+            StudentController _studentController = new StudentController();
+            AdminController _adminController = new AdminController();
 
-            ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "Welcome");
-            Console.WriteLine("---");
-            while (true)
+        Authentication: var admin = _adminController.Authenticate();
+            if (admin != null)
             {
-                ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "1- Create Group");
-                ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "2- Update Group");
-                ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "3- Delete Group");
-                ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "4- All Groups");
-                ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "5- Get Group By Name");
-                ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "6- Create Student");
-                ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "7- Update Student");
-                ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "8- Delete Student");
-                ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "9- All Students by Group");
-                ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "10- Get All Students by Group");
-                ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "0- Exit");
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "Welcome");
                 Console.WriteLine("---");
-                ConsoleHelper.WriteTextWithColor(ConsoleColor.Blue, "Select Option");
-                string number = Console.ReadLine();
-
-                int selectedNumber;
-                bool result = int.TryParse(number, out selectedNumber);
-                if(result)
+                while (true)
                 {
-                  if(selectedNumber >= 0 && selectedNumber <= 10)
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "1- Create Group");
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "2- Update Group");
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "3- Delete Group");
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "4- All Groups");
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "5- Get Group By Name");
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "6- Create Student");
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "7- Update Student");
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "8- Delete Student");
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "9- All Students by Group");
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "10- Get All Students by Group");
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "0- Exit");
+                    Console.WriteLine("---");
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Blue, "Select Option");
+                    string number = Console.ReadLine();
+
+                    int selectedNumber;
+                    bool result = int.TryParse(number, out selectedNumber);
+                    if (result)
                     {
-                        switch(selectedNumber)
+                        if (selectedNumber >= 0 && selectedNumber <= 10)
                         {
-                            #region CreateGroup
-                            case (int)Options.CreateGroup:
-                                _groupController.CreateGroup();
-                                break;
-                            #endregion
-                            case (int)Options.UptadeGroup:
-                                _groupController.UpdateGroup();
-                                break;
-                            case (int)Options.DeleteGroup:
-                                _groupController.DeleteGroup();
-                                break;
-                            #region AllGroups
-                            case (int)Options.AllGroups:
-                                _groupController.AllGroups();
-                                break;
-                            #endregion
-                            case (int)Options.GetGroupByName:
-                                _groupController.GetGroupByName();
-                                break;
-                            case (int)Options.Exit:
+                            switch (selectedNumber)
+                            {
+                                #region CreateGroup
+                                case (int)Options.CreateGroup:
+                                    _groupController.CreateGroup();
+                                    break;
+                                #endregion
+                                case (int)Options.UptadeGroup:
+                                    _groupController.UpdateGroup();
+                                    break;
+                                case (int)Options.DeleteGroup:
+                                    _groupController.DeleteGroup();
+                                    break;
+                                #region AllGroups
+                                case (int)Options.AllGroups:
+                                    _groupController.AllGroups();
+                                    break;
+                                #endregion
+                                case (int)Options.GetGroupByName:
+                                    _groupController.GetGroupByName();
+                                    break;
+                                case (int)Options.Exit:
 
-                               _groupController.Exit();
-                                break;
-                            case (int)Options.CreateStudent:
-                                _studentController.CreateStudent();
-                                break;
-                            case (int)Options.UptadeStudent:
-                                _studentController.UpdateStudent();
-                                break;
-                                case(int)Options.DeleteStudent:
-                                _studentController.DeleteStudent();
-                                break;
-                            case (int)Options.AllStudentsByGroup:
-                                _studentController.AllStudentsByGroup();
-                                break;
-                            case (int)Options.GetAllStudentsByGroup:
-                                _studentController.GetAllStudentsByGroup();
-                                break;
-                            
+                                    _groupController.Exit();
+                                    break;
+                                case (int)Options.CreateStudent:
+                                    _studentController.CreateStudent();
+                                    break;
+                                case (int)Options.UptadeStudent:
+                                    _studentController.UpdateStudent();
+                                    break;
+                                case (int)Options.DeleteStudent:
+                                    _studentController.DeleteStudent();
+                                    break;
+                                case (int)Options.AllStudentsByGroup:
+                                    _studentController.AllStudentsByGroup();
+                                    break;
+                                case (int)Options.GetAllStudentsByGroup:
+                                    _studentController.GetAllStudentsByGroup();
+                                    break;
 
-                               
+                            }
+
                         }
-                    }         
+                        else
+                        {
+                            ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, "Please, enter correct number");
+                        }
+                    }
+                    else
+                    {
+                        ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, "Please, enter correct number");
+                    }
+                
+                   
                 }
+
             }
-            
+            else
+            {
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, "Admin username or password is incorrect");
+                goto Authentication;
+
+            }
         }
     }
 }
--- DataAccess/Contexts/DbContext.cs
+++ DataAccess/Contexts/DbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Entities;
+using Core.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,8 +14,20 @@ namespace DataAccess.Contexts
         {
             Students = new List<Student>();
             Groups = new List<Group>();
+            Admins = new List<Admin>();
+
+            string password1 = "12345678";
+            var hashedPassword1 = PasswordHasher.Encrypt(password1);
+             Admin admin1 = new Admin("admin1", hashedPassword1);
+            Admins.Add(admin1);
+
+            string password2 = "87654321";
+            var hasheadPassword2 = PasswordHasher.Encrypt(password2);
+            Admin admin2 = new Admin("admin2", hasheadPassword2);
+            Admins.Add(admin2);
         }
         
+        public static List<Admin> Admins { get; set; }
         public static List<Student> Students { get; set; }
         public static List<Group> Groups { get; set; }
     }
